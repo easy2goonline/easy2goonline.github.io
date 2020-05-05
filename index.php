d54582506bc715dc6c2515a53ed20fceee8ae18b
MANJEET SINGH <47612774+manjeetsinghgec@users.noreply.github.com>
2020-05-06 01:45:30 +0530

    web

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE easygoDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+
+// sql to create table
+$dbname = "easygoDb";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "CREATE TABLE User (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table User created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+$conn->close();
+?>
